39a0e7dd123006a63291d0584514ad7219878ba0
udeshika <perera.n.u@gmail.com>
2023-11-05 17:53:58 +0800

    update libs

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-contract Counter {
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract Counter  is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
