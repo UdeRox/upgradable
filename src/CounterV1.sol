fce12b22d808cd10c9e3602ddfb4b1906c0e75c8
udeshika <perera.n.u@gmail.com>
2023-11-05 18:14:19 +0800

    Added new contract to upgradable contract

--- src/CounterV1.sol
+++ src/CounterV1.sol
@@ -5,8 +5,18 @@ import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Own
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract Counter  is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+contract CounterV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 public number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+        function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -15,4 +25,8 @@ contract Counter  is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     function increment() public {
         number++;
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal virtual override {}
 }
